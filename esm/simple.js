5438ba73583188975b17a5a22e1ab5e047907405
Domenic Horner <domenic@tgxn.net>
2023-01-17 19:17:10 +0800

    add simple example

---
+++ esm/simple.js
@@ -0,0 +1,28 @@
+import * as dotenv from "dotenv";
+
+import PushMeSDK, { Consts } from "@pushme-tgxn/pushmesdk";
+
+import main from "./main.js";
+
+dotenv.config({ path: `../.env` });
+let { TOPIC_SECRET, BACKEND_URL } = process.env;
+
+if (!TOPIC_SECRET || TOPIC_SECRET == "") {
+  throw new Error("Missing TOPIC_SECRET in .env");
+}
+
+if (!BACKEND_URL || BACKEND_URL == "") {
+  BACKEND_URL = null;
+}
+
+const pushmeClient = new PushMeSDK({
+  backendUrl: BACKEND_URL,
+});
+
+main(pushmeClient, TOPIC_SECRET, {
+  categoryId: Consts.PushCategory.SIMPLE,
+  title: "Wow, you're amazing!",
+  data: {
+    sendReceipt: true,
+  },
+});
